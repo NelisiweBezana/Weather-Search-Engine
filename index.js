bd704ddcd1849bc9679abd6dd23565a2399b3040
NelisiweBezana <140618126+NelisiweBezana@users.noreply.github.com>
2024-01-13 23:29:35 +0200

    API Integration

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ function handleSearchCity(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "6ot20ada7f719432a222baf96f0e9bb0";
   let forecastWeatherApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -70,24 +77,29 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌥️</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <div class="weather-forecast-icon"><img src="${
+            day.condition.icon_url
+          }" class="weather-forecast-icon" /></div>
           <div class="weather-forecast-temp">
             <span class="weather-forecast-temp-max">
-              <strong>15&deg</strong> /
+              <strong>${Math.round(day.temperature.maximum)}&deg</strong> /
             </span>
-            <span class="weather-forecast-temp-min">10&deg</span>
+            <span class="weather-forecast-temp-min">${Math.round(
+              day.temperature.maximum
+            )}&deg</span>
           </div>
         </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#weather-forecast");
